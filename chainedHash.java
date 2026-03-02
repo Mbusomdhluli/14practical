93a44a1a2d5caca23a04874b61924249b1b9d976
Mbusomdhluli <4577545@myuwc.ac.za>
2026-03-02 14:49:44 +0200

    Update chainedHash.java

--- chainedHash.java
+++ chainedHash.java
@@ -1,1 +1,34 @@
 //start
+public class chainedHash {
+
+    private static class Node {
+        String key;
+        String value;
+        Node next;
+
+        Node(String k, String v) {
+            key = k;
+            value = v;
+            next = null;
+        }
+    }
+  private Node[] table;
+    private int m;
+
+    public chainedHash(int m) {
+        this.m = m;
+        table = new Node[m + 1];
+    }
+
+    private int hash(String key) {
+        int h = Math.abs(key.hashCode());
+        return (h % m) + 1;
+    }
+
+    public boolean isEmpty() {
+        for (int i = 1; i <= m; i++)
+            if (table[i] != null)
+                return false;
+        return true;
+    }
+//progress
