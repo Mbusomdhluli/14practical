ba812d5b1b66005c84117087ea2f5750ef7cfd94
Mbusomdhluli <4577545@myuwc.ac.za>
2026-03-02 14:28:54 +0200

    Implement openHash class with Entry structure

--- openHash.java
+++ openHash.java
@@ -1,1 +1,24 @@
 //start
+public class openHash {
+
+    private static class Entry {
+        String key;
+        String value;
+        boolean deleted;
+
+        Entry(String k, String v) {
+            key = k;
+            value = v;
+            deleted = false;
+        }
+    }
+    private Entry[] table;
+    private int m;
+    private int size;
+
+    public openHash(int m) {
+        this.m = m;
+        table = new Entry[m + 1];
+        size = 0;
+    }
+  //progress
