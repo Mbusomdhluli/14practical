3b39c91cdf390927165f82b6cc9a8b9e12e17192
Mbusomdhluli <4577545@myuwc.ac.za>
2026-03-02 15:23:30 +0200

    Update practical14.java

--- practical14.java
+++ practical14.java
@@ -14,3 +14,32 @@ public class Practical14 {
         for (int i = 0; i < N; i++)
             keys[i] = String.valueOf(i + 1);
       //shaffle keys
+         
+        List<String> list = Arrays.asList(keys);
+        Collections.shuffle(list);
+        list.toArray(keys);
+
+        for (int i = 0; i < N; i++)
+            values[i] = String.valueOf(i + 1);
+
+        double[] loadFactors = {0.75, 0.80, 0.85, 0.90, 0.95};
+
+        System.out.println("Average Search Time (seconds)");
+        System.out.println("--------------------------------------------------");
+        System.out.println("Alpha\tN\tOpenHash\tChainedHash");
+
+        for (double alpha : loadFactors) {
+
+            int n = (int)(alpha * 1_000_000);
+            int m = (int)(n / alpha);
+
+            double openTime = 0;
+            double chainedTime = 0;
+
+            for (int r = 0; r < REPETITIONS; r++) {
+
+                openHash open = new openHash(m);
+                chainedHash chained = new chainedHash(m);
+
+//progress
+
